29c44e273356ee737b60140dfa62e731bf690068
Avalanche <dev@avalanche.li>
2025-12-08 21:21:01 +0100

    newupdate

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,25 +1,14 @@
 export default ({ env }) => ({
   upload: {
     config: {
-      provider: '@strapi/provider-upload-aws-s3',
+      provider: '@strapi/provider-upload-cloudinary',
       providerOptions: {
-        baseUrl: `https://${env('AWS_BUCKET')}.storage.railway.app`,
-        s3Options: {
-          credentials: {
-            accessKeyId: env('AWS_ACCESS_KEY_ID'),
-            secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
-          },
-          endpoint: env('AWS_ENDPOINT'),
-          region: env('AWS_REGION', 'auto'),
-          params: {
-            Bucket: env('AWS_BUCKET'),
-          },
-          forcePathStyle: false,
-        },
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
       },
       actionOptions: {
         upload: {},
-        uploadStream: {},
         delete: {},
       },
     },
