e2805470841977cb0bffab15848e073c67f19df4
Avalanche <dev@avalanche.li>
2025-12-08 21:46:31 +0100

    newupdate

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,16 +1,1 @@
-export default ({ env }) => ({
-  upload: {
-    config: {
-      provider: '@strapi/provider-upload-cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-      actionOptions: {
-        upload: {},
-        delete: {},
-      },
-    },
-  },
-});
+export default () => ({});
