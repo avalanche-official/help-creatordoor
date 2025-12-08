216144498b3c4a4f886ef683140ce9feb39c116f
Avalanche <dev@avalanche.li>
2025-12-08 20:39:11 +0100

    newupdate

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,1 +1,21 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: '@strapi/provider-upload-aws-s3',
+      providerOptions: {
+        s3Options: {
+          credentials: {
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
+          },
+          endpoint: env('AWS_ENDPOINT_URL'),
+          region: env('AWS_DEFAULT_REGION', 'auto'),
+          params: {
+            Bucket: env('AWS_S3_BUCKET_NAME'),
+          },
+          forcePathStyle: false,
+        },
+      },
+    },
+  },
+});
